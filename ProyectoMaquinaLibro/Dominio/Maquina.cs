ede5bf1db134d61fc2b5aba414fa01e8856aa025
Altair <jhangalvis422@gamil.com>
2022-03-15 22:43:56 -0500

    Primer update

---
+++ ProyectoMaquinaLibro/Dominio/Maquina.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMaquinaLibros
+{
+    public class Maquina
+    {
+        public List<Libro> Libros { get; set; }
+        public string Pago { get; set; }
+
+        public Maquina()
+        {
+            this.Libros = new List<Libro>();
+
+            Libro nombre = new Libro();
+            nombre.Codigo = "01";
+            nombre.Nombre = "Nombre1";
+            nombre.Categoria = "Fantasia";
+            nombre.Cantidad = 5;
+            nombre.Valor = 2000;
+
+            Libro nombre2 = new Libro();
+            nombre2.Codigo = "02";
+            nombre2.Nombre = "Nombre2";
+            nombre2.Categoria = "Ficcion";
+            nombre2.Cantidad = 2;
+            nombre2.Valor = 1500;
+
+            this.Libros.Add(nombre);
+            this.Libros.Add(nombre2);
+
+        }
+        //Validamos si la maquina ya tiene el libro
+        public int validaLibro(string codigo)
+        {
+            int encontro = -1;
+
+            for (int i = 0; i < this.Libros.Count; i++)
+            {
+                if (this.Libros[i].Codigo == codigo)
+                {
+                    encontro = i;
+                }
+            }
+
+            return encontro;
+        }
+
+        //Crear metodo "modificarLibro"
+
+        public bool agregarLibro(Libro libro)
+        {
+            int enc = this.validaLibro(libro.Codigo);
+            if (enc > 0)
+            {
+                this.Libros[enc].sumarCantidad(libro.Cantidad);
+            }
+            else
+            {
+                this.Libros.Add(libro);
+            }
+
+            return true;
+        }
+
+
+
+        //Elimina un libro de la maquina
+
+
+        public bool eliminarLibro(string codigo)
+        {
+            int enc = this.validaLibro(codigo);
+
+            if (enc >= 0)
+            {
+                this.Libros.RemoveAt(enc);
+                return true;
+            }
+
+            return false;
+        }
+
+        public double validarMonedas(string[] monedas)
+        {
+            double total = 0;
+            foreach (string item in monedas)
+            {
+                try
+                {
+                    total += float.Parse(item);
+                }
+                catch (Exception e) { }
+            }
+            return total;
+        }
+
+        // 1000-500-200-100
+        public Libro vender(string codigo)
+        {
+            int enc = this.validaLibro(codigo);
+            if (enc >= 0)
+            {
+                if (this.Libros[enc].validarCantidad())
+                {
+                    string[] monedas = this.Pago.Split('-');
+
+                    double total = this.validarMonedas(monedas);
+
+                    if (this.Libros[enc].validarValor(total))
+                    {
+                        this.Libros[enc].restarLibro();
+
+                        return this.Libros[enc];
+                    }
+                }
+            }
+            return null;
+        }
+
+        public string listarLibros()
+        {
+            string lista = "";
+            foreach (Libro item in this.Libros)
+            {
+                lista += item.Codigo + " " + item.Nombre + " " + item.Categoria + " " + item.Cantidad + " " + item.Valor + "\n";
+            }
+            return lista;
+
+        }
+    }
+}
---
+++ ProyectoMaquinaLibro/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppMaquinaLibros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maquina maquina = new Maquina();
+
+            while (true)
+            {
+                Console.WriteLine("Bienvenido a la maquina de libros");
+                Console.WriteLine(maquina.listarLibros());
+
+                Console.WriteLine("1. Agregar libro ");
+                Console.WriteLine("2. Modificar libro ");
+                Console.WriteLine("3. Eliminar libro ");
+                Console.WriteLine("4. Comprar libro ");
+                Console.WriteLine("Ingresa una opcion: ");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Libro libro = new Libro();
+                        Console.Write("Codigo: ");
+                        libro.Codigo = Console.ReadLine();
+
+                        Console.Write("Nombre: ");
+                        libro.Nombre = Console.ReadLine();
+
+                        Console.Write("Categoria: ");
+                        libro.Categoria = Console.ReadLine();
+
+                        Console.Write("Cantidad: ");
+                        libro.Cantidad = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Valor: ");
+                        libro.Valor = double.Parse(Console.ReadLine());
+
+                        maquina.agregarLibro(libro);
+                        Console.WriteLine(maquina.listarLibros());
+
+                        break;
+                    case "2":
+                        //modificar
+                        break;
+                    case "3":
+                        Console.Write("Codigo: ");
+                        string codigo = Console.ReadLine();
+
+                        maquina.eliminarLibro(codigo);
+                        Console.WriteLine(maquina.listarLibros());
+
+                        break;
+                    case "4":
+                        Console.Write("Codigo: ");
+                        string codigo_venta = Console.ReadLine();
+
+                        Console.Write("Monedas solo de (1000-500-200-100): ");
+                        maquina.Pago = Console.ReadLine();
+
+                        Libro lcomprado = maquina.vender(codigo_venta);
+
+                        if (lcomprado == null)
+                        {
+                            Console.WriteLine("No se pudo sacar el libro");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Su libro es {0} y la devuelta es {1}", lcomprado.Codigo, lcomprado.Cambio);
+                        }
+
+                        break;
+
+                }
+
+                Console.WriteLine("Desea continuar si/no: ");
+                if (Console.ReadLine() == "no")
+                {
+                    break;
+                }
+
+
+
+            }
+
+        }
+    }
+}
